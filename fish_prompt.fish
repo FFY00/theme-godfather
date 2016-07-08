d38c44336d551f706e01dc76ff1da1b515c28523
Nashwan Azhari <aznashwan@yahoo.com>
2016-07-08 17:58:28 +0300

    Made prompt more consistent in git repos.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -43,7 +43,7 @@ function fish_prompt
     if [ (_is_git_dirty) ]
       set git_info $red $git_branch "×"
     else
-      set git_info $green $git_branch
+      set git_info $green $git_branch " "
     end
     echo -n -s ' ' $git_info $normal
   end
